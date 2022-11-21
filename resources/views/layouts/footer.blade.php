170c6794ba0a43c65fddc707e2b393fa03e744a6
AsakaLynux <asakalynux@gmail.com>
2022-11-21 14:52:44 +0700

    add layouts header and footer

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+</head>
+<body>
+    <div class="footer">
+        <h1>
+            &#169 Happy Book Store 2022
+        </h1>
+    </div>
+</body>
+</html>
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+
+</head>
+<body>
+    <div class="header">
+        <div id="titleHeader">
+            <h1>Giant Book Supplier</h1>
+        </div>
+
+    </div>
+    <div class="navbar">
+        <a href="#home">Home</a>
+
+        <div class="dropdown">
+          <button class="dropbtn">Category
+            <i class="fa fa-caret-down"></i>
+          </button>
+          <div class="dropdown-content">
+            <a href="#">Category 1</a>
+            <a href="#">Category 2</a>
+            <a href="#">Category 3</a>
+          </div>
+        </div>
+        <a href="#Publisher">Publisher</a>
+        <a href="#contact">Contact</a>
+      </div>
+</body>
+</html>
