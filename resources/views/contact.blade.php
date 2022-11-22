bb85e7c84bbc0d7f8a053d173dbf009cb9a010e9
AsakaLynux <asakalynux@gmail.com>
2022-11-22 17:24:06 +0700

    make static page for contact page

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -3,5 +3,27 @@
 @section('titleName', 'Giant Book Supplier')
 
 @section('content')
-    <h1>This is Contact</h1>
+    <div class="contactHeader">
+        <h1>Contact</h1>
+    </div>
+    <div class="address">
+        <ul>
+            <h1>Store Address: </h1>
+            <li>Perumahan Bukit Cengkeh 3</li>
+            <li>Depok, Jawa Barat</li>
+            <li>Indonesia</li>
+        </ul>
+        br
+        <ul>
+            <h1>Open Daily: </h1>
+            <li>Never Close</li>
+        </ul>
+        <br>
+        <ul>
+            <h1>Contact</h1>
+            <li>Phone: 123468324684</li>
+            <li>Email: asdkfje@gmail.cim</li>
+        </ul>
+
+    </div>
 @endsection
