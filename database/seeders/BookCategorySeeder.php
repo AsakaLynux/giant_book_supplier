6f2529a3eced18c1d2589c57ed7449d4077b0486
AsakaLynux <asakalynux@gmail.com>
2022-11-23 11:06:59 +0700

    add relation to database

--- database/seeders/BookCategorySeeder.php
+++ database/seeders/BookCategorySeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class BookCategorySeeder extends Seeder
 {
@@ -14,5 +15,95 @@ class BookCategorySeeder extends Seeder
     public function run()
     {
         //
+        DB::table('book_categories')->insert([
+            [
+                'book_id' => 1,
+                'category_id' => 1
+            ],
+            [
+                'book_id' => 1,
+                'category_id' => 2
+            ],
+            [
+                'book_id' => 1,
+                'category_id' => 5
+            ],
+            [
+                'book_id' => 1,
+                'category_id' => 6
+            ],
+            [
+                'book_id' => 1,
+                'category_id' => 7
+            ],
+            [
+                'book_id' => 2,
+                'category_id' => 1
+            ],
+            [
+                'book_id' => 2,
+                'category_id' => 2
+            ],
+            [
+                'book_id' => 2,
+                'category_id' => 3
+            ],
+            [
+                'book_id' => 2,
+                'category_id' => 6
+            ],
+            [
+                'book_id' => 3,
+                'category_id' => 1
+            ],
+            [
+                'book_id' => 3,
+                'category_id' => 2
+            ],
+            [
+                'book_id' => 3,
+                'category_id' => 6
+            ],
+            [
+                'book_id' => 3,
+                'category_id' => 7
+            ],
+            [
+                'book_id' => 4,
+                'category_id' => 1
+            ],
+            [
+                'book_id' => 4,
+                'category_id' => 2
+            ],
+            [
+                'book_id' => 4,
+                'category_id' => 6
+            ],
+            [
+                'book_id' => 5,
+                'category_id' => 4
+            ],
+            [
+                'book_id' => 5,
+                'category_id' => 6
+            ],
+            [
+                'book_id' => 6,
+                'category_id' => 1
+            ],
+            [
+                'book_id' => 6,
+                'category_id' => 2
+            ],
+            [
+                'book_id' => 6,
+                'category_id' => 6
+            ],
+            [
+                'book_id' => 6,
+                'category_id' => 7
+            ],
+        ]);
     }
 }
