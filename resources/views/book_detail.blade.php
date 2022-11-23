76418f1539529eff4e71b9f1fadc105566ccb28f
AsakaLynux <asakalynux@gmail.com>
2022-11-23 08:46:10 +0700

    Update css

--- resources/views/book_detail.blade.php
+++ resources/views/book_detail.blade.php
@@ -3,15 +3,17 @@
 @section('titleName', 'Giant Book Supplier')
 
 @section('content')
-    <h1>This is Book Detail</h1>
-    <br>
-    <img src={{$books->image}} alt="Avatar" style="width:100px">
+
+    <div class="detail">
+        <img src={{$books->image}} alt="Avatar" style="width:200px">
         <div class="container">
-          <h4><b>{{$books->title}}</b></h4>
+          <h4><b>Title: {{$books->title}}</b></h4>
+          <h4><b>Author: {{$books->author}}</b></h4>
+          <h4><b>Year: {{$books->year}}</b></h4>
+          {{-- <h4><b>Publisher: {{$books->publisher}}</b></h4> --}}
           <br>
-          <h4>By</h4>
-          <br>
-          <h4><b>{{$books->author}}</b></h4>
-          {{-- <p>Architect & Engineer</p> --}}
+          <h5><strong>Synopsis:</strong></h5>
+          <p>{{$books->synopsis}}</p>
         </div>
+    </div>
 @endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,18 +3,19 @@
 @section('titleName', 'Giant Book Publisher')
 
 @section('content')
-    @foreach ($books as $book)
+    <div class="cover">
+        @foreach ($books as $book)
     <div class="card">
-        <img src={{$book->image}} alt="Avatar" style="width:100px">
+        <img src={{$book->image}} alt="Avatar" style="width:160px">
         <div class="container">
           <h4><b>{{$book->title}}</b></h4>
-          <br>
           <h4>By</h4>
-          <br>
           <h4><b>{{$book->author}}</b></h4>
           {{-- <p>Architect & Engineer</p> --}}
         </div>
+        <br>
         <a href="{{ url('/book_detail/'.$book->id) }}"><button>Detail</button></a>
       </div>
     @endforeach
+    </div>
 @endsection
